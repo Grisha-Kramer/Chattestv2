084cc36b5ac272316fd7dc53a5bda96abfd914c1
Grisha Kramer <gregorykramer@HAL-8998.local>
2020-05-02 15:57:52 -0400

    this works, next level does not

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import { Widget } from "react-chat-widget";
 import "react-chat-widget/lib/styles.css";
 
 function App() {
+  const handleNewUserMessage = (newMessage) => {
+    console.log(`New message incoming! ${newMessage}`);
+    // Now send the message throught the backend API
+  };
+
   return (
     <div className="App">
-      <Widget />
+      <Widget handleNewUserMessage={handleNewUserMessage} />
     </div>
   );
 }
